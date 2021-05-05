202cb8af0258ec9f3c5ddb5d6807fa685bfba881
hamhead1005 <82336485+hamhead1005@users.noreply.github.com>
2021-05-04 21:52:48 -0700

    Add files via upload
    Latest Version of Expert System. All cases for 60+ should be implemented along with a few from the other age groups. Refer to Knowledge Base excel file for missing implementations.
    
    I anticipate having to add more and edit the 60+ category but for the most part I think its ready.

--- Case_Severity.pl
+++ Case_Severity.pl
@@ -1,3 +1,14 @@
+%D = Result
+%A = Age (60 = 60+)
+%SR = Runny-Nose
+%SC = Cough
+%SF = Fever
+%SB = Difficuly-Breathing
+%PO = Obesity
+%PI = Immunocompromised
+%PH = Heart-Condition
+%PL = Lung-Disease
+
 check(D) :-
     write('What is your age range?: '),
     read(A),
@@ -17,7 +28,9 @@ check(D) :-
     read(PH),
     write('Have you been diagnosed with a Lung Disease?: '),
     read(PL), nl,
-    write('If "Low" you are ok to stay home. If "Mild" call a Doctor. If "Severe" go to the hospital'),
+    write('If "Low" you are ok to stay home.'), nl,
+    write('If "Mild" call a Doctor.'), nl,
+    write('If "Severe" go to the hospital.'), nl,
     severity(D,A,SR,SC,SF,SB,PO,PI,PH,PL).
 
 %Mapping for those with "Low" Severity%
@@ -31,20 +44,119 @@ severity('Low',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
     (PO = y; PO = n),
     PI = n,
     PH = n,
-    PL = n,!.
+    PL = n,!. %Tested
+
+severity('Low',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(0-9) ; A=(10-30) ; A=(30-59)),
+    (SR = n; SR=y),
+    (SC = n; SC = y),
+    SF = n,
+    SB = n,
+    (PO = y; PO = n),
+    PI = n,
+   (PH = n; PH = y),
+    PL = n,!. %Tested
+
+severity('Low',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(0-9) ; A=(10-30) ; A=(30-59)),
+    (SR = n; SR=y),
+    (SC = n),
+    SF = n,
+    SB = n,
+    (PO = y; PO = n),
+    (PI = n; PI = y),
+   (PH = n; PH = y),
+    (PL = n; PL = y),!.
+
+severity('Low',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(0-9)),
+    (SR = n; SR=y),
+    (SC = n; SC = y),
+    (SF = n; SF = y),
+    (SB = n),
+    (PO = y; PO = n),
+    (PI = n),
+    (PH = n),
+    (PL = n),!.
 
 %Mapping for those with "Mild" Severity%
 
 severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
-    (A=(10-30) ; A=(30-59) ; A=(60)),
-    (SR = n; SR = y),
-    SC = y,
-    (SF = y; SR = n),
-    SB = n,
-    (PO = n; PO = y),
-    PI = n,
-    PH = n,
-    PL = n,!.
+    (A=(0-9)),
+    (SR = n; SR=y),
+    (SC = y),
+    (SF = y),
+    (SB = n),
+    (PO = y; PO = n),
+    (PI = n; PI = y),
+    (PH = n; PI = y),
+    (PL = n),!.
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(10-30)),
+    (SR=y),
+    (SC = y),
+    (SF = y),
+    (SB = n),
+    (PO = y; PO = n),
+    (PI = n),
+    (PH = n),
+    (PL = n),!.
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(10-30)),
+    (SR=y),
+    (SC = y),
+    (SF = y),
+    (SB = n),
+    (PO = y; PO = n),
+    (PI = n),
+    (PH = y),
+    (PL = n),!. %tested
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(30-59)),
+    (SR=y; SR = n),
+    (SC = y),
+    (SF = y),
+    (SB = n),
+    (PO = y; PO = n),
+    (PI = y),
+    (PH = n; PH = y),
+    (PL = n),!. %tested
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(60)),
+    (SR=y; SR = n),
+    (SC = y),
+    (SF = n),
+    (SB = n),
+    (PO = y; PO = n),
+    (PI = n),
+    (PH = n),
+    (PL = n),!.
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(60)),
+    (SR=y),
+    (SC = y; SC = n),
+    (SF = n),
+    (SB = n),
+    (PO = y),
+    (PI = y),
+    (PH = n),
+    (PL = n),!.
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    (A=(30-59); A = 60),
+    (SR=y),
+    (SC = y),
+    (SF = n),
+    (SB = n),
+    (PO = y),
+    (PI = y),
+    (PH = y),
+    (PL = n; PL = y),!.
 
 %Mapping for those with "Severe" Severity%
 
@@ -57,4 +169,117 @@ severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
     (PO = y; PO = n),
     (PI = n; PI = y),
     (PH = n; PH = y),
-    PL = y,!.
+    PL = y,!. %Tested
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR=y),
+    (SC = y; SC = n),
+    (SF = y),
+    (SB = y; SB = n),
+    (PO = y),
+    (PI = y; PI = n),
+    (PH = y),
+    (PL = n),!. %Tested
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR=y; SR = n),
+    (SC = y),
+    (SF = y; SF = n),
+    (SB = y),
+    (PO = y; PO = n),
+    (PI = y; PI = n),
+    (PH = y; PH = n),
+    (PL = y),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR = n),
+    (SC = n),
+    (SF = y),
+    (SB = y; SB = n),
+    (PO = y; PO = n),
+    (PI = y),
+    (PH = n),
+    (PL = n),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR = n; SR = y),
+    (SC = y),
+    (SF = y; SF = n),
+    (SB = y),
+    (PO = y; PO = n),
+    (PI = y),
+    (PH = y),
+    (PL = n; PL = y),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR = n; SR = y),
+    (SC = y; SC = n),
+    (SF = y; SF = n),
+    (SB = y),
+    (PO = y; PO = n),
+    (PI = y; PI = n),
+    (PH = y; PH = n),
+    (PL = n; PL = y),!.
+
+% Fever with at least 1 pre Condition (60)
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR = n; SR = y),
+    (SC = y; SC = n),
+    (SF = y),
+    (SB = n),
+    (PO = y),
+    (PI = n),
+    (PH = n),
+    (PL = n),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR = n; SR = y),
+    (SC = y; SC = n),
+    (SF = y),
+    (SB = n),
+    (PO = n),
+    (PI = y),
+    (PH = n),
+    (PL = n),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR = n; SR = y),
+    (SC = y; SC = n),
+    (SF = y),
+    (SB = n),
+    (PO = n),
+    (PI = n),
+    (PH = y),
+    (PL = n),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(60),
+    (SR = n; SR = y),
+    (SC = y; SC = n),
+    (SF = y),
+    (SB = n),
+    (PO = n),
+    (PI = n),
+    (PH = n),
+    (PL = y),!.
+%end Fever with 1 PreCondition (60)
+
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(0-9),
+    (SR=y),
+    (SC = y),
+    (SF = y),
+    (SB = y),
+    (PO = y; PO = n),
+    (PI = y),
+    (PH = n; PH = y),
+    (PL = y),!. %Tested
