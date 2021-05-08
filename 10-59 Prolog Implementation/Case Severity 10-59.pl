c3f7d347778c5b8666d5a2cfa40d3152cf8d6aed
DannyHoCPP <72366232+DannyHoCPP@users.noreply.github.com>
2021-05-08 13:08:03 -0700

    10-59 case implementation

---
+++ 10-59 Prolog Implementation/Case Severity 10-59.pl
@@ -0,0 +1,116 @@
+%D = Result
+%A = Age (60 = 60+)
+%SR = Runny-Nose
+%SC = Cough
+%SF = Fever
+%SB = Difficuly-Breathing
+%PO = Obesity
+%PI = Immunocompromised
+%PH = Heart-Condition
+%PL = Lung-Disease
+
+check(D) :-
+    write('What is your age range?: '),
+    read(A),
+    write('Do you have a Runny Nose?: '),
+    read(SR),
+    write('Do you have a Cough?: '),
+    read(SC),
+    write('Do you have a Fever?: '),
+    read(SF),
+    write('Do you have Difficulty Breathing?: '),
+    read(SB),
+    write('Have you been diagnosed with Obesity?: '),
+    read(PO),
+    write('Have you been diagnosed as Immunocompromised?: '),
+    read(PI),
+    write('Have you been diagnosed with a Heart Condition?: '),
+    read(PH),
+    write('Have you been diagnosed with a Lung Disease?: '),
+    read(PL), nl,
+    write('If "Low" you are ok to stay home.'), nl,
+    write('If "Mild" call a Doctor.'), nl,
+    write('If "Severe" go to the hospital.'), nl,
+    severity(D,A,SR,SC,SF,SB,PO,PI,PH,PL).
+
+%Mapping for those with "Mild" Severity%
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(10-59),
+    (SR = y),
+    (SC = y),
+    (SF = y),
+    (SB = n),
+    (PO = n ; PO = y),
+    (PI = n),
+    (PH = n ; PH = y),
+    (PL = n),!.
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(10-59),
+    (SR = n ; SR = y),
+    (SC = y),
+    (SF = y),
+    (SB = n),
+    (PO = n ; PO = y),
+    (PI = n),
+    (PH = y),
+    (PL = n),!.
+
+severity('Mild',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(10-59),
+    (SR = y),
+    (SC = y),
+    (SF = n),
+    (SB = n),
+    (PO = y),
+    (PI = y),
+    (PH = y),
+    (PL = n ; PL = y),!.
+
+%Mapping for those with "Severe" Severity%
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(10-59),
+    (SR = n),
+    (SC = y),
+    (SF = n),
+    (SB = y),
+    (PO = n ; PO = y),
+    (PI = n),
+    (PH = n),
+    (PL = y),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(10-59),
+    (SR = n ; SR = y),
+    (SC = y),
+    (SF = y),
+    (SB = y),
+    (PO = n ; PO = y),
+    (PI = n ; PI = y),
+    (PH = n ; PH = y),
+    (PL = n ; PL = y),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(10-59),
+    (SR = y),
+    (SC = n ; SC = y),
+    (SF = y),
+    (SB = y),
+    (PO = n ; PO = y),
+    (PI = y),
+    (PH = n),
+    (PL = n),!.
+
+severity('Severe',A,SR,SC,SF,SB,PO,PI,PH,PL) :-
+    A=(10-59),
+    (SR = y),
+    (SC = n),
+    (SF = y),
+    (SB = n),
+    (PO = y),
+    (PI = y),
+    (PH = y),
+    (PL = y),!.
+
